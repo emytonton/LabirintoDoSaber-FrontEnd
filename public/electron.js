b3eb950f060541db44679b04dcc6a3ce7a44c93f
Emilly Paiva <emillypaiva3260@gmail.com>
2025-10-15 20:11:06 -0300

    chore: electron

---
+++ public/electron.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('is-dev');
+
+function createWindow() {
+  
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+ 
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  win.loadURL(startURL);
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+
+app.whenReady().then(createWindow);
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
